aa8f9ac59ead500b38e9215ce4cddc785f4b13bf
Aslan Mammadov <mammadovaslan21@gmail.com>
2023-07-22 14:14:13 +0400

    swipe events fix

--- main.js
+++ main.js
@@ -10,8 +10,6 @@ class SlideStories {
     this.touchStartY = 0;
     this.touchEndX = 0;
     this.touchEndY = 0;
-    this.swipeThreshold = 100;
-    this.isSwiping = false;
 
     this.slide.addEventListener("touchstart", this.handleTouchStart.bind(this), false);
     this.slide.addEventListener("touchmove", this.handleTouchMove.bind(this), false);
@@ -39,32 +37,18 @@ class SlideStories {
     this.touchEndY = event.touches[0].clientY;
   }
 
-  handleTouchStart(event) {
-    this.touchStartX = event.touches[0].clientX;
-    this.touchStartY = event.touches[0].clientY;
-  }
-
-  handleTouchMove(event) {
-    event.preventDefault();
-    this.touchEndX = event.touches[0].clientX;
-    this.touchEndY = event.touches[0].clientY;
-  }
-
   handleTouchEnd(event) {
     const deltaX = this.touchEndX - this.touchStartX;
     const deltaY = this.touchEndY - this.touchStartY;
 
-    if (Math.abs(deltaX) > this.swipeThreshold && Math.abs(deltaX) > Math.abs(deltaY)) {
+    const touchThreshold = 50;
+
+    if (Math.abs(deltaX) > touchThreshold && Math.abs(deltaX) > Math.abs(deltaY)) {
       if (deltaX > 0) {
         this.prev();
       } else {
         this.next();
       }
-
-      this.isSwiping = true;
-      setTimeout(() => {
-        this.isSwiping = false;
-      }, 500); // Set a delay to allow time for the click event to be processed
     }
   }
 
